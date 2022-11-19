848bea4a6db193e5b959ee50573c7290a7726127
kamal00yyu <52659066+kamal00yyu@users.noreply.github.com>
2022-11-19 18:19:18 +0530

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-function onClickMenu() {
-    document.getElementById("menu-hamberger").classList.toggle("change");
-    document.getElementById("nav-hamberger").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-
-}
